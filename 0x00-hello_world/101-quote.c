b26b2e2819578929fce93d32e884d61fee26b509
loubna <loubinette779@gmail.com>
2023-02-16 10:11:32 -0800

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,13 @@
 
 /**
  * main - A c program that prints a line to the standard error
- * Description: prints Dora's quote
- * Return: 1 (Success)
+ *
+ * Return:1 (Success)
  */
+
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
+
