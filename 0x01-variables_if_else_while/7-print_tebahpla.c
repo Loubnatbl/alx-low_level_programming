b0d88ea45e5e09220c01dcb3c51ce1ac784d165d
loubna <loubinette779@gmail.com>
2023-02-17 08:03:18 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	charalp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
