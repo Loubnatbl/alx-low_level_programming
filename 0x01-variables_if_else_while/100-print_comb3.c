5664827d2cb83693b89ad8190d280b44d764f2bc
loubna <loubinette779@gmail.com>
2023-02-17 09:35:10 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (i < 100)
+	{
+		j = i % 10;
+		k = i / 10;
+
+		if (j > k)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
