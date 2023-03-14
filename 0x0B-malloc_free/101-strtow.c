2e65ca67419796331b502cd567f1b3ae18877c42
loubna <loubinette779@gmail.com>
2023-03-14 15:15:51 -0700

    malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,80 +1,101 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * word_count - function to calculate number of words
- * @str: string being passed to check for words
+ * wordcount - get word count from string
+ *             without spaces
  *
- * Return: number of words
- */
-int word_count(char *str)
-{
-	int count;
-	int in_word;
+ * @str: string to count words present
+ *
+ * Return: The number of words
+*/
 
-	count = 0;
-	in_word = 0;
+int wordcount(char *str)
+{
+	int words = 0;
+
 	while (*str != '\0')
 	{
-		if (*str == ' ')
-			in_word = 0;
-		else if (in_word == 0)
-		{
-			count++;
-			in_word = 1;
-		}
-		str++;
-	}
-	return (count);
-}
-void free_memo(char **s, int i)
-{
-	int j;
-
-	for (j = 0; j < i; j++)
-		free(s[j]);
-	free(s);
-}
-char **strtow(char *str)
-{
-	int j, c, word_len, words;
-	char **arr;
-	char *word_start;
-
-	if (str == NULL || *str == ' ')
-		return (NULL);
-	words = word_count(str);
-
-	if (words == 0)
-		return (NULL);
-	arr = malloc((words + 1) * sizeof(char *));
-	if (arr == NULL)
-		return (NULL);
-	for (j = 0; str[j] != '\0'; j++)
-	{
-		word_len = 0;
+		/*skip spaces*/
 		if (*str == ' ')
 			str++;
 		else
 		{
-			word_start = str;
+			/*count words*/
 			while (*str != ' ' && *str != '\0')
-			{
-				word_len++;
 				str++;
-			}
-			arr[j] = malloc((word_len + 1) * sizeof(char));
-
-			if (arr[j] == NULL)
-			{
-				free_memo(arr, j);
-				return (NULL);
-			}
-			for (c = 0; word_start[c] != ' ' && word_start[c] != '\0'; c++)
-				arr[j][c] = word_start[c];
-			arr[j][c] = '\0';
+			words++;
 		}
 	}
-	arr[j] = NULL;
-	return (arr);
+	return (words);
+}
+
+/**
+ * free_array - free arr[i]
+ *
+ * @ar: array to free
+ * @i: array[i]
+ *
+ * Return: nothing
+*/
+
+void free_array(char **ar, int i)
+{
+	if (ar != NULL && i != 0)
+	{
+		while (i >= 0)
+		{
+			free(ar[i]);
+			i--;
+		}
+		free(ar);
+	}
+}
+
+/**
+ * strtow - split a string to words
+ *
+ * @str: string to split.
+ *
+ * Return: NULL if it fails
+*/
+
+char **strtow(char *str)
+{
+	int i, s, j, str_l, word;
+	char **string;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	str_l = wordcount(str);
+	/*return null if str_l == 0 || new == NULL*/
+	string = malloc((str_l + 1) * sizeof(char *));
+	if (str_l == 0 || string == NULL)
+		return (NULL);
+
+	for (i = s = 0; i < str_l; i++)
+	{
+		for (word = s; str[word] != '\0'; word++)
+		{
+			if (str[word] == ' ')
+				s++;
+
+			if (str[word] != ' ' && (str[word + 1] == ' ' || str[word + 1] == '\0'))
+			{
+				string[i] = malloc((word - s + 2) * sizeof(char));
+				if (string[i] == NULL)
+				{
+					free_array(string, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+
+		for (j = 0; s <= word; s++, j++)
+			string[i][j] = str[s];
+		string[i][j] = '\0';
+	}
+	string[i] = NULL;
+	return (string);
 }
